ce3c1ceef587dca3ef6aefcf1d655489242cd8bb
Carlo Cayos <carlo.cayos@sandstone.com.au>
2017-07-04 03:00:47 +1000

    More commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 <h2>Swagger UI</h2>
 http://localhost:8080/swagger-ui.html
+
+<h2>Running via Maven</h2>
+mvn spring-boot:run
+
+<h2>Swagger Spring Profile</h2>
+Remove Spring Profile to disable Swagger
+spring.profiles.active=swagger
--- src/main/java/com/carlocayos/configuration/SwaggerConfig.java
+++ src/main/java/com/carlocayos/configuration/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.carlocayos.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -11,6 +12,7 @@ import springfox.documentation.swagger.web.UiConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@Profile("swagger")
 @EnableSwagger2
 public class SwaggerConfig
 {
